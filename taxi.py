2a1ca4c4be5fdcc58a2f8e2c2e87c91d0ad20c85
vcuino1 <46705795+vcuino1@users.noreply.github.com>
2019-08-01 19:46:03 +0800

    Update taxi.py

--- taxi.py
+++ taxi.py
@@ -35,8 +35,8 @@ def epsilon_greedy_policy(state, epsilon):
         return max(list(range(env.action_space.n)), key=lambda x: Q[(state, x)])
 
 
-# 训练1000个episode
-for i in range(1000):
+# 训练5000个episode
+for i in range(5000):
     r = 0
     # 初始化状态（env.reset()用于重置环境）
     state = env.reset()
@@ -58,5 +58,29 @@ for i in range(1000):
         if done:
             break
     # 打印当前episode的奖励
+    print("[Episode %d] Total reward: %d" % (i + 1, r))
+#测试100个数据
+for i in range(100):
+    r = 0
+    # 初始化状态（env.reset()用于重置环境）
+    state = env.reset()
+    # 一个episode
+    while True:
+        # 输出当前agent和environment的状态（可删除）
+        env.render()
+        # 采用ε-贪婪策略选取动作
+        action = epsilon_greedy_policy(state, epsilon)
+        # 执行动作，得到一些信息
+        nextstate, reward, done, _ = env.step(action)
+        # 更新Q表
+        update_q_table(state, action, reward, nextstate, alpha, gamma)
+        # s ⬅ s'
+        state = nextstate
+        # 累加奖励
+        r += reward
+        # 判断episode是否到达最终状态
+        if r>=0:
+            break
+             # 打印当前episode的奖励
     print("[Episode %d] Total reward: %d" % (i + 1, r))
 env.close()
